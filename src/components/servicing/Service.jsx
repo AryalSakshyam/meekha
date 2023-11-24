3266666bff1b7bedf42a6191851cf128362504dd
AryalSakshyam <sakshyam150@gmail.com>
2023-11-24 18:44:20 +0545

    Second Push

--- src/components/servicing/Service.jsx
+++ src/components/servicing/Service.jsx
@@ -2,51 +2,64 @@ import React from 'react'
 import image1 from '../../assets/office.jpg'
 import image2 from '../../assets/design.jpg'
 import image3 from '../../assets/app.jpg'
-import image4 from '../../assets/web.jpeg'
+import image4 from '../../assets/web.jpg'
 
 
 
-const Service = () => {
+const Service = () => { 
+
+    
   return (
     <>
-    <div className='py-20 px-10 grid grid-cols-4 gap-10'>
+    <div className='p-10 grid grid-cols-4 gap-10 font-mulish'>
         <div className=''>
-            <div>
-                <img className='w-full h-full' src={image1} alt="" />
+                
+            <div className='mb-5 bg-gradient-to-b from-black to-[red]'>
+                <img className='  w-full h-full rounded-t-[40px] rounded-br-3xl' src={image1} alt="" />
             </div>
-            <div>
-                <h1>IT Consulting</h1>
-            </div>
-            <div>dfdsfdsfsd</div>
-        </div>
-        <div className=''>
             <div >
-                <img className='w-full h-full' src={image2} alt="" />
+                <h1 className='py-5 font-bold text-xl '>IT Consulting</h1>
             </div>
             <div>
-                <h1>UI/UX Design</h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sed ipsum, porro a, accusamus vero quo alias sint quaerat qui corrupti tempore quibusdam perferendis debitis odit assumenda illo suscipit nobis!
+                </p>
             </div>
-            <div>dcdscds</div>
         </div>
         <div>
-            <div>
-                <img className='w-full h-full ' src={image3} alt="" />
+            <div className='mb-5 '>
+                <img className='w-full h-full rounded-t-[40px] rounded-br-3xl bg-gradient-bg-black-to-bg-white' src={image2} alt="" />
             </div>
             <div>
-                <h1>
+                <h1 className='py-5 font-bold text-xl ' >UI/UX Design</h1>
+            </div>
+            <div><p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sed ipsum, porro a, accusamus vero quo alias sint quaerat qui corrupti tempore quibusdam perferendis debitis odit assumenda illo suscipit nobis!
+                </p></div>
+        </div>
+        <div>
+            <div className='mb-5 '>
+                <img className='w-full h-full rounded-t-[40px] rounded-br-3xl ' src={image3} alt="" />
+            </div>
+            <div>
+                <h1 className='py-5 font-bold text-xl '>
                     App Development
                 </h1>
             </div>
-            <div></div>
+            <div><p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sed ipsum, porro a, accusamus vero quo alias sint quaerat qui corrupti tempore quibusdam perferendis debitis odit assumenda illo suscipit nobis!
+                </p></div>
         </div>
         <div>
-            <div>
-                <img className='w-full h-full' src={image4} alt="" />
+            <div className='mb-5'>
+                <img className='w-full h-full rounded-t-[40px] rounded-br-3xl' src={image4} alt="" />
             </div>
             <div>
-                <h1>Web Development</h1>
+                <h1 className='py-5 font-bold text-xl '>Web Development</h1>
             </div>
-            <div></div>
+            <div><p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sed ipsum, porro a, accusamus vero quo alias sint quaerat qui corrupti tempore quibusdam perferendis debitis odit assumenda illo suscipit nobis!
+                </p></div>
         </div>
     </div>
     </>
